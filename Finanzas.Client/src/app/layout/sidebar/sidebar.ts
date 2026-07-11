d35fde5ef52061013778c7ea8e3917014272c19b
Braylin20 <braylin_vasquez@ucne.edu.do>
2026-07-11 10:51:39 -0400

    Hace el sidebar responsive

--- Finanzas.Client/src/app/layout/sidebar/sidebar.ts
+++ Finanzas.Client/src/app/layout/sidebar/sidebar.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
 })
-export class Sidebar {}
+export class Sidebar {
+  public isOpen = false;
+
+  public toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+  public close() {
+    this.isOpen = false;
+  }
+}
