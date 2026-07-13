6cb92d61a48fea0d1ecd4db8c5095f98154d4f8f
Braylin20 <braylin_vasquez@ucne.edu.do>
2026-07-13 15:24:39 -0400

    Agrega swagger para production

--- Finanzas.API/Finanzas/Program.cs
+++ Finanzas.API/Finanzas/Program.cs
@@ -29,13 +29,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    
-}
+
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 app.UseCors("CorsPolicy");
